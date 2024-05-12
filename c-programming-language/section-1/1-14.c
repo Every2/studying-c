d0fbfe30485a841a7b64ee2dcd4e8de2c4f80de0
Every <christiansantoslima21@gmail.com>
2024-05-12 03:49:33 -0300

    finish section 1.5 of book

---
+++ c-programming-language/section-1/1-14.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+main() {
+    int c;
+    
+    int chars[26];
+
+    for (int i = 0; i < 26; ++i) {
+        chars[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            chars[c - 'a']++;
+        }
+    }
+    
+    for (int i = 0; i < 26; ++i) {
+        printf("%2c | ", 'a' + i); 
+        for (int j = 0; j < chars[i]; ++j) {
+            putchar('='); 
+        }
+        putchar('\n');
+    }
+    
+}
