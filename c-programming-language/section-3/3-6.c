8e99a6c1a7a8afc88d25554a81c773787bb2cb02
Every <christiansantoslima21@gmail.com>
2024-08-24 21:44:42 -0300

    exercise 3-6

---
+++ c-programming-language/section-3/3-6.c
@@ -0,0 +1,36 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void itoa(int n, char s[], int width) {
+    int i, sign;
+
+    sign = n;
+    i = 0;
+    do {
+        s[i++] = abs(n % 10) + '0';
+    } while (n /= 10);
+
+    if (sign < 0) s[i++] = '-';
+    while (i < width) s[i++] = ' ';
+    s[i] = '\0';
+    reverse(s);
+}
+
+int main(void) {
+    char s[50];
+    itoa(INT_MIN, s, 10);
+    printf("%s\n", s);
+
+    return 0;
+}
